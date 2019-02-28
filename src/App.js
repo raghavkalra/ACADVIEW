d7f764fed371bcb785ba26a50ce6efef4b82c8d6
raghavkalra <raghav1664.cse16@chitkara.edu.in>
2019-02-28 11:43:24 +0530

    Ex:cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Display from './Display';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main">
+        <h1>Hi, I am React App!</h1>
+        <br/>
+        <div className="cards">
+            <Display name='John' age='31' birthMonth='June' birthYear='1998'/>
+            <input type="text"></input>
+        </div>
+        <br/>
+        <div className="cards">
+            <Display name='Rick' age='45' birthMonth='July' birthYear='1996'/>
+            <input type="text"></input>
+        </div>
       </div>
     );
   }
